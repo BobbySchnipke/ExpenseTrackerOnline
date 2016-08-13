5751f6a27c61215633562d2ef2499b31d6a56c4b
BobbySchnipke <bobbyschnipke@gmail.com>
2016-08-13 13:14:02 -0400

    Add project files.

---
+++ ExpenseTrackerOnline/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExpenseTrackerOnline.Startup))]
+namespace ExpenseTrackerOnline
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
